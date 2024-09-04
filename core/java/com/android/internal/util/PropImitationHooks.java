beae0f270de17fe9ad47c10e6f2a76e54dca4935
Karan Parashar <karan@pixelos.net>
2024-09-04 08:22:16 +0200

    PIHooks: Update fingerprints to September release
    Change-Id: Iaf7c3ab31d8624d3341fe0d675679e1492049db5
    Signed-off-by: Saikrishna1504 <saikrishna26918@gmail.com>

--- core/java/com/android/internal/util/PropImitationHooks.java
+++ core/java/com/android/internal/util/PropImitationHooks.java
@@ -77,15 +77,15 @@ public class PropImitationHooks {
     private static final ComponentName GMS_ADD_ACCOUNT_ACTIVITY = ComponentName.unflattenFromString(
             "com.google.android.gms/.auth.uiflows.minutemaid.MinuteMaidActivity");
 
-    private static final Map<String, String> sPixelEightProps = Map.of(
-            "PRODUCT", "husky",
-            "DEVICE", "husky",
-            "HARDWARE", "husky",
+    private static final Map<String, String> sPixelNineProps = Map.of(
+            "PRODUCT", "caiman",
+            "DEVICE", "caiman",
+            "HARDWARE", "caiman",
             "MANUFACTURER", "Google",
             "BRAND", "google",
-            "MODEL", "Pixel 8 Pro",
-            "ID", "AP2A.240705.005",
-            "FINGERPRINT", "google/husky/husky:14/AP2A.240705.005/11942872:user/release-keys"
+            "MODEL", "Pixel 9 Pro",
+            "ID", "AD1A.240905.004",
+            "FINGERPRINT", "google/caiman/caiman:14/AD1A.240905.004/12196292:user/release-keys"
     );
 
     private static final Map<String, String> sPixelFiveProps = Map.of(
@@ -95,8 +95,8 @@ public class PropImitationHooks {
             "MANUFACTURER", "Google",
             "BRAND", "google",
             "MODEL", "Pixel 5a",
-            "ID", "AP2A.240705.004",
-            "FINGERPRINT", "google/barbet/barbet:14/AP2A.240705.004/11875680:user/release-keys"
+            "ID", "AP2A.240805.004",
+            "FINGERPRINT", "google/barbet/barbet:14/AP2A.240805.005/12025142:user/release-keys"
     );
 
     private static final Map<String, String> sPixelTabletProps = Map.of(
@@ -106,8 +106,8 @@ public class PropImitationHooks {
             "MANUFACTURER", "Google",
             "BRAND", "google",
             "MODEL", "Pixel Tablet",
-            "ID", "AP2A.240705.005",
-            "FINGERPRINT", "google/tangorpro/tangorpro:14/AP2A.240705.005/11942872:user/release-keys"
+            "ID", "AP2A.240905.003",
+            "FINGERPRINT", "google/tangorpro/tangorpro:14/AP2A.240905.003/12231197:user/release-keys"
     );
 
     private static final Map<String, String> sPixelXLProps = Map.of(
@@ -231,8 +231,8 @@ public class PropImitationHooks {
                     dlog("Spoofing Pixel Tablet for: " + packageName + " process: " + processName);
                     setProps(sPixelTabletProps);
                 } else {
-                    dlog("Spoofing Pixel 8 Pro for: " + packageName + " process: " + processName);
-                    setProps(sPixelEightProps);
+                    dlog("Spoofing Pixel 9 Pro for: " + packageName + " process: " + processName);
+                    setProps(sPixelNineProps);
                 }
                 return;
             case PACKAGE_GPHOTOS:
